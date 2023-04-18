8692c3792133e63785134c08a4b17b36c8794a4a
Muhamadfikri20 <muhamadfikri9602@gmail.com>
2023-04-18 09:57:14 +0700

    [upd]using_named_routes_and_passing_data_with_named_routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './category_meals_screen.dart';
 import './categories_screen.dart';
 
 void main() {
@@ -23,6 +24,7 @@ class MyApp extends StatelessWidget {
             titleMedium: TextStyle(fontSize: 20, fontFamily: 'RobotoCondensed', fontWeight: FontWeight.bold)),
       ),
       home: CategoriesScreen(),
+      routes: {'/category-meals': (ctx) => CategoryMealsScreen()},
     );
   }
 }
