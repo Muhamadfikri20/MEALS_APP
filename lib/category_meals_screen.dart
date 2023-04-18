e8d25c22e964f60390b650e5fb7b790b81db0f07
Muhamadfikri20 <muhamadfikri9602@gmail.com>
2023-04-18 13:43:36 +0700

    [UPD]adding_a_meal_model_and_data

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -10,11 +10,6 @@ class CategoryMealsScreen extends StatelessWidget {
     final routeArgs = ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'];
     final categoryId = routeArgs['id'];
-    return Scaffold(
-      appBar: AppBar(title: Text(categoryTitle)),
-      body: Center(
-        child: Text('The Recipe For The Category'),
-      ),
-    );
+    return Scaffold(appBar: AppBar(title: Text(categoryTitle)), body: ListView.builder(itemBuilder: (ctx, index) {}));
   }
 }
