613194b532d6ea28d196362b39d3fb80082558ec
Muhamadfikri20 <muhamadfikri9602@gmail.com>
2023-05-03 16:02:29 +0700

    [upd]navigating_to_the_meal_detail_page

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import '../screens/meal_detail_screen.dart';
 import '../models/meal.dart';
 
 class MealItem extends StatelessWidget {
+  final String id;
   final String title;
   final String imageUrl;
   final int duration;
   final Complexity complexity;
   final Affordability affordability;
-  MealItem({@required this.title, @required this.imageUrl, @required this.affordability, @required this.complexity, @required this.duration});
+  MealItem({@required this.id, @required this.title, @required this.imageUrl, @required this.affordability, @required this.complexity, @required this.duration});
   String get complexityText {
     switch (complexity) {
       case Complexity.Simple:
@@ -40,11 +42,14 @@ class MealItem extends StatelessWidget {
     }
   }
 
-  void selectMeal() {}
+  void selectMeal(BuildContext context) {
+    Navigator.of(context).pushNamed(MealDetailScreen.routeName, arguments: id);
+  }
+
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: selectMeal,
+      onTap: () => selectMeal(context),
       child: Card(
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(15),
